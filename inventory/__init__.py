2c2532d9480d54ebb13a1312090d490186409431
formatt18 <mtf62@cornell.edu>
2021-04-25 19:10:21 -0400

    added initilization from approx constant order policy, made new registered env

--- inventory/__init__.py
+++ inventory/__init__.py
@@ -1,6 +1,12 @@
 from gym.envs.registration import register
 
+#register(
+#    id='inventory-v0',
+#    entry_point='inventory.envs:Inventory',
+#)
+
 register(
-    id='inventory-v0',
+    id='inventory-v1',
     entry_point='inventory.envs:Inventory',
+    kwargs={'length':  500}
 )
